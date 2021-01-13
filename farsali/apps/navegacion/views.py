d4436298460f14e0e93243402b2709046317fe57
Karina <kary_1806@hotmail.com>
2021-01-13 11:20:24 -0500

    Fix range product

--- farsali/apps/navegacion/views.py
+++ farsali/apps/navegacion/views.py
@@ -163,10 +163,8 @@ def productsView(request):
     productos_qs = Producto.objects.filter(**filter_kwargs)[int(pagination):int(pagination)+quantity]
     productos = productos_qs.values(*fields, **map_fields)
     for p in productos:
-        print("/"*30)
-        print(p['calificacion'])
-        p['calificacion_cantidad'] = range(
-            0, int(p['calificacion'])+1) if p.get('calificacion') else []
+        p['calificacion_cantidad'] = list(range(
+            0, p['calificacion']+1)) if p.get('calificacion') else []
     return JsonResponse({'error':False,'data':list(productos)}, status=200 ,safe=False)
 
 
